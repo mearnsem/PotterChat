a1d465786447a2ed25366483846a5e59306f937e
Emily Mearns <mearnsem@gmail.com>
2016-06-29 15:49:07 -0600

    Added some swift files
    Getting a skeleton set up

---
+++ PotterChat/PotterChat/HouseController.swift
@@ -0,0 +1,77 @@
+//
+//  HouseController.swift
+//  PotterChat
+//
+//  Created by Emily Mearns on 6/29/16.
+//  Copyright © 2016 Emily Mearns. All rights reserved.
+//
+
+import Foundation
+import CloudKit
+
+class HouseController {
+    
+    static let sharedController = HouseController()
+    
+    func addMessageToHouse() {
+        
+    }
+    
+    func deleteMessageFromHouse() {
+        
+    }
+    
+    func getMessageForHouse() {
+        
+    }
+    
+    func saveContext() {
+        
+    }
+    
+    // MARK: - Subscriptions
+    
+    func subscribeToNewMessages() {
+        
+    }
+    
+    func checkSubscriptionToMessages() {
+        
+    }
+    
+    func addSubscriptionToMessages() {
+        
+    }
+    
+    func removeSubscriptionToMessages() {
+        
+    }
+    
+    func toggleMessageSubscription() {
+        
+    }
+    
+    // MARK: - Helper Functions
+    
+    func syncedRecords() {
+        
+    }
+    
+    func unsyncedRecords() {
+        
+    }
+    
+    func fetchNewRecords() {
+        
+    }
+    
+    func pushChangesToCloudKit() {
+        
+    }
+    
+    func performFullSync() {
+        
+    }
+    
+    
+}
---
+++ PotterChat/PotterChat/UserController.swift
@@ -0,0 +1,15 @@
+//
+//  UserController.swift
+//  PotterChat
+//
+//  Created by Emily Mearns on 6/29/16.
+//  Copyright © 2016 Emily Mearns. All rights reserved.
+//
+
+import Foundation
+
+class UserController {
+    
+    
+    
+}
